d1b2b00ce77d8f67a8490fbf151ff932d64701c5
brunitob <brunitob@gmail.com>
2019-09-07 21:23:34 -0400

    Thanks page fix

--- client/src/pages/Thanks/index.js
+++ client/src/pages/Thanks/index.js
@@ -9,6 +9,7 @@ import './styles.scss';
 import Layout from '../../components/Layout';
 import kingOfLobstersSmall from '../../assets/images/king-of-lobsters-small.png';
 import badgePoap from '../../assets/images/poap-badge.png';
+import dai from '../../assets/images/dai-2.svg';
 
 class Thanks extends Component {
   submitVote = () => {
@@ -26,7 +27,7 @@ class Thanks extends Component {
           <div className="thanks">
             <h1>Congratulations!</h1>
             <img src={badgePoap} alt="King of Lobsters" />
-            <h2>You are participating for 500 dai!</h2>
+            <h2 className="subtitle">You are participating for 500 <img src={dai} alt="DAI" /></h2>
           </div>
         </div>
       </Layout>
